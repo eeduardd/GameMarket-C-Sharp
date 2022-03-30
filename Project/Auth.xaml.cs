0535d7e89e47feffce419822ddd84fd5096b04ee
eeduardd <54077091+eeduardd@users.noreply.github.com>
2022-03-31 01:26:40 +0300

    Update

---
+++ Project/Auth.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for Auth.xaml
+    /// </summary>
+    public partial class Auth : Window
+    {
+        MainWindow mainWindow = new MainWindow();
+        public Auth()
+        {
+            InitializeComponent();
+        }
+        private void Click_Authorization(object sender, RoutedEventArgs e)
+        {
+            if (textHost.Text == mainWindow.Host
+                & textUsername.Text == mainWindow.UserName
+                & textDBname.Text == mainWindow.DBName
+                & textPassword.Password == mainWindow.Password
+                & textPort.Text == mainWindow.Port)
+            {
+                this.Close();
+                mainWindow.Show();
+            }
+            else MessageBox.Show("Введите корректные данные!");
+        }
+    }
+}
--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Project
 {
@@ -21,36 +23,94 @@ namespace Project
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static string Host = "localhost";
-        private static string Username = "postgres";
-        private static string DBname = "GameMarket";
-        private static string Password = "ICO3v$LN#KM2w";
-        private static string Port = "5432";
+        private readonly static string host = "localhost";
+        private readonly static string userName = "postgres";
+        private readonly static string dBName = "GameMarket";
+        private readonly static string password = "ICO3v$LN#KM2w";
+        private readonly static string port = "5432";
+
+        public string Host { get { return host; } set { Host = value; } }
+        public string UserName { get { return userName; } set { UserName = value; } }
+        public string DBName { get { return dBName; } set { DBName = value; } }
+        public string Password { get { return password; } set { Password = value; } }
+        public string Port { get { return port; } set { Port = value; } }
+
 
         // Build connection string using parameters from portal
-        string connString =
+        static readonly string connString =
             String.Format(
-                $"Server={Host};" +
-                $"Username={Username};" +
-                $"Database={DBname};" +
-                $"Port={Port};" +
-                $"Password={Password}");
+                $"Server={host};" +
+                $"Username={userName};" +
+                $"Database={dBName};" +
+                $"Port={port};" +
+                $"Password={password}");
+
+        Npgsql.NpgsqlConnection conn = new NpgsqlConnection(connString);
+        List<string> table_names = new List<string>();
+        DataTable dataTable = new DataTable();
+        DataTable tables = new DataTable();
+        NpgsqlDataAdapter adapter = new NpgsqlDataAdapter();
         public MainWindow()
         {
             InitializeComponent();
+
+            // получение данных о всех таблицах
+            tables = conn.GetSchema("Tables");
+
+            foreach (DataRow row in tables.Rows)
+                table_names.Add(Convert.ToString(row["table_name"]));
+
+            foreach (string item in table_names)
+                loadDataList.Items.Add(item);
         }
 
-        private void Button_Load_Database(object sender, RoutedEventArgs e)
+        private void Load_DataBase_Button(object sender, RoutedEventArgs e)
         {
-            using (Npgsql.NpgsqlConnection conn = new NpgsqlConnection(connString))
-            {
-                conn.Open();
-                if (conn.State == System.Data.ConnectionState.Open)
-                    MessageBox.Show("Connected");
+            conn.Open();
 
-                NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM ", conn);
-                cmd.ExecuteNonQuery();
+            adapter = new NpgsqlDataAdapter($"select * from {loadDataList.SelectedItem}", conn);
+
+            adapter.Fill(dataTable);
+            adapter.Update(dataTable);
+
+            dataGrid.ItemsSource = dataTable.AsDataView();
+
+            conn.Close();
+        }
+
+        private void Update_Data_Button(object sender, RoutedEventArgs e)
+        {
+            conn.Open();
+
+            try
+            {
+                NpgsqlCommandBuilder comandbuilder = new NpgsqlCommandBuilder(adapter);
+                adapter.Update(dataTable);
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            conn.Close();
+        }
+
+        private void ContextMenu_Delete_Button(object sender, RoutedEventArgs e)
+        {
+            conn.Open();
+
+            try
+            {
+                dataTable.Rows.RemoveAt(dataGrid.SelectedIndex);
+                adapter.Update(dataTable);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            conn.Close();
         }
     }
+
 }
